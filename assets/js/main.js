573bf3381ce0fd67a14ee539037cab269cd37889
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2021-08-25 17:59:55 +0300

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,12 +112,14 @@ window.onscroll = function showHeader() {
             scrollPos = (document.body.getBoundingClientRect()).top;
 }
 
-const modalBrn = document.querySelectorAll('[data-modal]')
+const modalBtn = document.querySelectorAll('[data-modal]')
 const modalClose = document.querySelectorAll('[data-modal-close]')
+
+const modalCloseScroll = document.querySelectorAll('[data-modal-close-scroll]')
 const modal = document.querySelectorAll('.c-modal')
 const body = document.body
 
-modalBrn.forEach(item => {
+modalBtn.forEach(item => {
     item.addEventListener('click', event => {
         let $this = event.currentTarget
         let modalId = $this.getAttribute('data-modal')
@@ -128,12 +130,14 @@ modalBrn.forEach(item => {
         modalContent.addEventListener('click', event => {
             event.stopPropagation()
         })
+        setTimeout(() => {
+            body.classList.add('no-scroll')
+        }, 100)
 
-         
-              modal.classList.add('show')
-        body.classList.add('no-scroll')
+        modal.classList.add('show')
 
-      
+
+
         setTimeout(() => {
             modalContent.style.transform = "none"
             modalContent.style.opacity = "1"
@@ -154,6 +158,12 @@ modal.forEach(item => {
         closeModal(currentModal)
     })
 })
+modalCloseScroll.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget.closest('.c-modal')
+        closeModalWithScroll(currentModal)
+    })
+})
 
 function closeModal(currentModal) {
     let modalContent = currentModal.querySelector('.c-modal__content')
@@ -162,7 +172,16 @@ function closeModal(currentModal) {
         currentModal.classList.remove('show')
         body.classList.remove('no-scroll')
     }, 100)
-        modalContent.removeAttribute('style')
+    modalContent.removeAttribute('style')
+
+}
+function closeModalWithScroll(currentModal) {
+    let modalContent = currentModal.querySelector('.c-modal__content')
+
+    setTimeout(() => {
+        currentModal.classList.remove('show')
+    }, 100)
+    modalContent.removeAttribute('style')
 
 }
 const burger = document.getElementById('sidebarToggle')
