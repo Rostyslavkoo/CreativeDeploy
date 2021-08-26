6ff3e2e34397617d75f39f308f2570bf77aabb16
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2021-08-26 19:22:43 +0300

    j

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,9 +94,9 @@ window.onscroll = function showHeader() {
     let headerimg = document.querySelector('#header-img')
     let headerfixedimg = document.querySelector('#header-fixed-img')
 
-    if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    if (pageYOffset < scrollPos) {
 
-        if (window.pageYOffset > 100) {
+        if (window.pageYOffset > 700) {
             header.classList.add('c-header__fixed')
             headerimg.style.display = "none";
             headerfixedimg.style.display = "block";
@@ -111,8 +111,9 @@ window.onscroll = function showHeader() {
             headerimg.style.display = "block";
             headerfixedimg.style.display = "none";
     }
-            scrollPos = (document.body.getBoundingClientRect()).top;
+            scrollPos = pageYOffset
 }
+// (document.body.getBoundingClientRect()).top;
 
 const modalBtn = document.querySelectorAll('[data-modal]')
 const modalClose = document.querySelectorAll('[data-modal-close]')
@@ -223,6 +224,62 @@ wrapper.addEventListener('click', (event) => {
     sidebar.classList.remove('active-sidebar')
     document.body.classList.remove('active-burger')
 })
+const animItems = document.querySelectorAll('.c-anim-items')
+
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll)
+
+    function animOnScroll() {
+        for (let i = 0; i < animItems.length; i++) {
+            const animItem = animItems[i]; // об'єкт
+            const animItemHeight = animItem.offsetHeight;// висота об'єкта
+            const animItemOffset = offset(animItem).top // позиція об'єкта
+            const animItemStart = 4; // коефіцієнт
+
+            let animItemPoint = window.innerHeight - animItemHeight / animItemStart // від висоти браузера віднімаю висоту елемента
+            if (animItemHeight > window.innerHeight) { //  перевірка чи висота об'єкта не більша за висоту робочого екрану
+                let animItemPoint = window.innerHeight - window.innerHeight / animItemStart
+            }
+            if ((pageYOffset > animItemOffset - animItemPoint) && (pageYOffset < animItemOffset + animItemPoint)) {
+                animItem.classList.add('_active')
+            } else {
+                if (!animItem.classList.contains('c-anim-hide')) {
+                    animItem.classList.remove('_active')
+                }
+            }
+
+
+        }
+    }
+
+
+
+
+    let preloader = document.querySelector('.c-preloader');
+    body.onload = function() {
+        setTimeout(() => {
+            body.classList.add('c-hide-preloader')
+            body.classList.remove('preloader-active')
+
+            setTimeout(function() {
+                body.classList.add('c-preloader-hidden')
+                body.classList.remove('no-scroll')
+                animOnScroll()
+            }, 100)
+
+        }, 999)
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+        return { top: rect.top + scrollTop }
+    }
+
+}
+
     const txt = ['Креатив...']
     const cursor = document.querySelector('.c-text-cursor').innerHTML
     const cursorObj = document.querySelector('.c-text-cursor')
@@ -260,22 +317,6 @@ wrapper.addEventListener('click', (event) => {
     }
     typeText();
 
-    let preloader = document.querySelector('.c-preloader');
-    body.onload = function() {
-        setTimeout(() => {
-            body.classList.add('c-hide-preloader')
-                body.classList.remove('preloader-active')
 
-            setTimeout(function() {
-                body.classList.add('c-preloader-hidden')
-                body.classList.remove('no-scroll')
-                setTimeout(() => {
-                    body.style.transform = "none"
-                    body.style.opacity = "1"
-                }, 1)
-            }, 900)
-
-        }, 999)
-    }
 
 
